101cf457ac83d86a8079233225cceb55f397309e
Scott <Scott@Mimsy.domain>
2016-02-21 13:23:10 -0700

    Version 3 final version - getHistory updated to reflect operation history. Test has been added in JUnit test class to reflect change.

--- src/cse360assign3/Calculator.java
+++ src/cse360assign3/Calculator.java
@@ -1,55 +1,99 @@
+/**
+ * Class performs basic calculator operations on total including
+ * add, subtract, multiply, and divide. Calculator also returns a history
+ * of operation performed.
+ * 
+ * @author Scott Moore @version 3  
+ * */
 package cse360assign3;
 
 public class Calculator {
 
 	private int total;
 	
+	private String history;
+	
 	public Calculator () {
 		
 		total = 0;  // not needed - included for clarity
 		
+		history = "0"; // initializes history string to 0 for base case
+		
 	}
 	
+	/**Returns the total calculated by Calculator methods called. */
 	public int getTotal () {
 		
-		return total;
+		return total; 
 		
 	}
 	
+	/**
+	 * Adds value passed to total and updates history to reflect operation performed.
+	 * 
+	 * @param value	Value added to the total, history string updated with " + " followed by value
+	 */
 	public void add (int value) {
 		
 		total += value;
 		
+		history = history + " + " + value;
+		
 	}
 	
+	/**
+	 * Subtracts value passed to total and updates history to reflect operation performed.
+	 * 
+	 * @param value Value subtracted from the total, history string updated with " - " followed by value
+	 */
 	public void subtract (int value) {
 		
 		total -= value;
 		
+		history = history + " - " + value;
+		
 	}
 	
+	/**
+	 * Multiplies value passed to total and updates history to reflect operation performed.
+	 * 
+	 * @param value Value multiplied by the total, history string updated with " * " followed by value
+	 */
 	public void multiply (int value) {
 		
 		total *= value;
 		
+		history = history + " * " + value;
+		
 	}
 	
+	/**
+	 * Divides value passed to total and updates history to reflect operation performed.
+	 * 
+	 * @param value Value divided by the total, history string updated with " / " followed by value
+	 */
 	public void divide (int value) {
 		
-		if (value == 0){
+		if (value == 0){ //if dividing by 0 make total 0
 			
 			total = 0;
 			
 		}
-		else{
+		else{ //else divide total by passed value
 			
 			total /= value;
 		
 		}
 		
+		history = history + " / " + value;
 	}
 	
+	/**
+	 * Contains a record of operations in string form  ex. "0 + 1 * 2"
+	 * @return history 
+	 */
 	public String getHistory () {
-		return "";
+		
+		return history;
 	}
 }
--- src/cse360assign3/CalculatorTest.java
+++ src/cse360assign3/CalculatorTest.java
@@ -1,4 +1,11 @@
+/**
+ * Class tests basic Calculator method operations, constructor is tested as well as
+ * addition, subtraction, multiplication, division and history.
+ * 
+ * @author Scott Moore @version 3  
+ * */
 package cse360assign3;
+
 
 import static org.junit.Assert.*;
 
@@ -12,7 +19,7 @@ public class CalculatorTest {
 		
 		Calculator calc = new Calculator();
 		
-		assertNotNull(calc);
+		assertNotNull(calc); //calc private variables initializes as 0 not NULL
 	}
 	
 	@Test
@@ -20,7 +27,7 @@ public class CalculatorTest {
 		
 		Calculator calc = new Calculator();
 		
-		assertEquals(calc.getTotal(), 0);
+		assertEquals(calc.getTotal(), 0); //constructor initializes total to 0 expected value 0
 		
 	}
 	
@@ -31,11 +38,11 @@ public class CalculatorTest {
 		
 		calc.add(4);
 		
-		assertEquals(calc.getTotal(), 4);
+		assertEquals(calc.getTotal(), 4); // 4 added to initialized total of 0
 		
 		calc.add(5);
 		
-		assertEquals(calc.getTotal(), 9);
+		assertEquals(calc.getTotal(), 9);// 5 added to previous total value of 4 equaling 9
 		
 	}
 	
@@ -44,15 +51,15 @@ public class CalculatorTest {
 		
 		Calculator calc = new Calculator();
 		
-		calc.add(4);
+		calc.add(4); //4 added to total value of 0 to test subtraction
 		
 		calc.subtract(2);
 		
-		assertEquals(calc.getTotal(), 2);
+		assertEquals(calc.getTotal(), 2);// 2 subtracted from 4 to equal 2
 		
 		calc.subtract(1);
 		
-		assertEquals(calc.getTotal(), 1);	
+		assertEquals(calc.getTotal(), 1);// 1 subtracted from 2 to equal 1
 		
 	}
 	
@@ -61,15 +68,15 @@ public class CalculatorTest {
 		
 		Calculator calc = new Calculator();
 		
-		calc.add(1);
+		calc.add(1); //1 added to total of 0 to test multiplication
 		
 		calc.multiply(5);
 		
-		assertEquals(calc.getTotal(), 5);
+		assertEquals(calc.getTotal(), 5);//1 multiplied by 5 equaling 5
 		
 		calc.multiply(5);
 		
-		assertEquals(calc.getTotal(), 25);
+		assertEquals(calc.getTotal(), 25);//5 multiplied by 5 equaling 25
 			
 	}
 	
@@ -78,15 +85,15 @@ public class CalculatorTest {
 		
 		Calculator calc = new Calculator();
 		
-		calc.add(10);
+		calc.add(10);//10 added to total of 0 to test division
 		
 		calc.divide(5);
 		
-		assertEquals(calc.getTotal(), 2);
+		assertEquals(calc.getTotal(), 2);//10 divided by 5 to equal 2
 		
 		calc.divide(0);
 		
-		assertEquals(calc.getTotal(), 0);
+		assertEquals(calc.getTotal(), 0);//2 divided by 0 to equal 0 per special case in calculator class
 		
 		
 	}
@@ -96,7 +103,17 @@ public class CalculatorTest {
 		
 		Calculator calc = new Calculator();
 		
-		assertEquals(calc.getHistory(),"");
+		calc.add(2); //2 added to test history "0 + 2"
+		
+		calc.subtract(1);//1 subtracted to test history "0 + 2 - 1"
+		
+		calc.divide(0);//0 divided to test history "0 + 2 - 1 / 0"
+		
+		calc.add(5);//5 added to test history " 0 + 2 - 1 / 0 + 5"
+		
+		calc.multiply(2);//2 multiplied to test history "0 + 2 - 1 / 0 + 5 * 2"
+		
+		assertEquals(calc.getHistory(),"0 + 2 - 1 / 0 + 5 * 2");
 	}
 	
 }
