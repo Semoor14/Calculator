e2ab7fbe99e1efa034401d95c675e30d65f57e89
Scott <Scott@Mimsy.domain>
2016-02-20 17:28:27 -0700

    Version 2, contains functioning calculator methods with appropriate test cases (excludes getHistory) getHistory Still returns an empty string.

--- src/cse360assign3/Calculator.java
+++ src/cse360assign3/Calculator.java
@@ -5,26 +5,47 @@ public class Calculator {
 	private int total;
 	
 	public Calculator () {
+		
 		total = 0;  // not needed - included for clarity
+		
 	}
 	
 	public int getTotal () {
-		return 0;
+		
+		return total;
+		
 	}
 	
 	public void add (int value) {
+		
+		total += value;
 		
 	}
 	
 	public void subtract (int value) {
 		
+		total -= value;
+		
 	}
 	
 	public void multiply (int value) {
 		
+		total *= value;
+		
 	}
 	
 	public void divide (int value) {
+		
+		if (value == 0){
+			
+			total = 0;
+			
+		}
+		else{
+			
+			total /= value;
+		
+		}
 		
 	}
 	
--- src/cse360assign3/CalculatorTest.java
+++ src/cse360assign3/CalculatorTest.java
@@ -6,10 +6,6 @@ import org.junit.Test;
 
 public class CalculatorTest {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
 	
 	@Test
 	public void testCalculator(){
@@ -22,30 +18,83 @@ public class CalculatorTest {
 	@Test
 	public void testGetTotal(){
 		
+		Calculator calc = new Calculator();
+		
+		assertEquals(calc.getTotal(), 0);
+		
 	}
 	
 	@Test
 	public void testAdd(){
+		
+		Calculator calc = new Calculator();
+		
+		calc.add(4);
+		
+		assertEquals(calc.getTotal(), 4);
+		
+		calc.add(5);
+		
+		assertEquals(calc.getTotal(), 9);
 		
 	}
 	
 	@Test
 	public void testSubtract(){
 		
+		Calculator calc = new Calculator();
+		
+		calc.add(4);
+		
+		calc.subtract(2);
+		
+		assertEquals(calc.getTotal(), 2);
+		
+		calc.subtract(1);
+		
+		assertEquals(calc.getTotal(), 1);	
+		
 	}
 	
 	@Test
 	public void testMultiply(){
 		
+		Calculator calc = new Calculator();
+		
+		calc.add(1);
+		
+		calc.multiply(5);
+		
+		assertEquals(calc.getTotal(), 5);
+		
+		calc.multiply(5);
+		
+		assertEquals(calc.getTotal(), 25);
+			
 	}
 	
 	@Test
 	public void testDivide(){
 		
+		Calculator calc = new Calculator();
+		
+		calc.add(10);
+		
+		calc.divide(5);
+		
+		assertEquals(calc.getTotal(), 2);
+		
+		calc.divide(0);
+		
+		assertEquals(calc.getTotal(), 0);
+		
+		
 	}
 	
 	@Test
 	public void testGetHistory(){
+		
+		Calculator calc = new Calculator();
 		
 	}
 	
